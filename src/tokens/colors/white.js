910a6d0b20d42a2de501fcbc0abb9c9da436f3fb
Rishav Bharti <rishavbharti21@gmail.com>
2022-11-11 13:18:45 +0530

    feat: update white token

--- src/tokens/colors/white.js
+++ src/tokens/colors/white.js
@@ -2,13 +2,20 @@ const shades = {
   0: '#FFFFFF',
   10: '#FCFCFC',
   20: '#F5F5F5',
-  30: '#EFEFEF',
-  40: '#E8E8E8',
+  30: '#F0F0F0',
+  40: '#EBEBEB',
   50: '#DFDFDF',
+};
+
+const outline = {
+  10: '#E8E8E8',
+  20: '#DFDFDF',
+  30: '#CFCFCF',
 };
 
 const white = {
   ...shades,
+  ...outline,
   main: shades[0],
   light: shades[20],
 };
